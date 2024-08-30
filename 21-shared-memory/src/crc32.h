8fa26527836ad350d895e1e1d08c1dd2bd698a08
manaldush <manaldush@gmail.com>
2024-08-30 11:42:19 +0100

    21-shared-memory \ stabilization

--- 21-shared-memory/src/crc32.h
+++ 21-shared-memory/src/crc32.h
@@ -1,4 +1,6 @@
 #pragma once
-#include <stdint.h>
 
-uint32_t crc32(uint32_t crc, const void *buf, size_t size);
+#include <stdint.h>
+#include <stddef.h>
+
+uint32_t crc32c(uint32_t crc, const void *buf, size_t size);
--- 21-shared-memory/src/main.c
+++ 21-shared-memory/src/main.c
@@ -1,7 +1,17 @@
 #include <stdlib.h>
 #include <sys/mman.h>
 #include <sys/stat.h>
+#include "crc32.h"
+#include <stdio.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <unistd.h>
 
+//4mb
+#define PAGE_SIZE 4194304
+#define handle_error(msg) \
+    do { perror(msg); } while (0)
 
 int main(int argc, char** argv) {
 
@@ -15,6 +25,7 @@ int main(int argc, char** argv) {
 
     int fd = open(argv[1], O_RDONLY);
     if (fd == -1) {
+        perror("File opening");
         goto exitPoint;
     }
 
@@ -25,10 +36,30 @@ int main(int argc, char** argv) {
     }
     
     off_t pageSize = sysconf(_SC_PAGE_SIZE);
+    while(pageSize < PAGE_SIZE) {
+        pageSize = pageSize << 1;
+    }
+    printf("Page size %ld\n", (long)pageSize);
+    void* addr = NULL;
 
-//    addr = mmap(NULL, length + offset - pa_offset, PROT_READ, MAP_PRIVATE, fd, pa_offset);
-//    if (addr == MAP_FAILED)
-//        handle_error("mmap");
+    uint32_t crcValue = 0;
+
+    for(off_t counter = 0; counter < sb.st_size; counter+=pageSize) {
+        size_t length = pageSize;
+        if ((sb.st_size - counter) < pageSize) {
+            length = sb.st_size - counter;
+        }
+        addr = mmap(addr, length, PROT_READ, MAP_PRIVATE, fd, counter);
+        if (addr == MAP_FAILED) {
+            handle_error("mmap");
+            result = EXIT_FAILURE;
+            goto closeFilePoint;
+        }
+        crcValue = crc32c(crcValue, addr, length);
+    }
+
+    printf("Result value is %lu\n", (unsigned long)crcValue);
+
 
 closeFilePoint:
     close(fd);
